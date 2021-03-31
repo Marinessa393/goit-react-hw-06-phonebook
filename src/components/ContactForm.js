5333bbc473a34941db01ab3999fecf2aadf07e6a
Marinessa393 <futurism1303@gmail.com>
2021-03-31 21:17:27 +0300

    add validation

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -52,6 +52,9 @@ export default class ContactForm extends Component {
             name="name"
             value={this.state.name}
             onChange={this.handleChange}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+            required
           />
         </label>
         <label>
@@ -61,6 +64,9 @@ export default class ContactForm extends Component {
             name="number"
             value={this.state.number}
             onChange={this.handleChange}
+            pattern="(\+?( |-|\.)?\d{1,2}( |-|\.)?)?(\(?\d{3}\)?|\d{3})( |-|\.)?(\d{3}( |-|\.)?\d{4})"
+            title="Номер телефона должен состоять из 11-12 цифр и может содержать цифры, пробелы, тире, пузатые скобки и может начинаться с +"
+            required
           />
         </label>
         <button type="submit" className="form__btn">
