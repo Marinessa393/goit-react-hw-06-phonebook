bc240382cd157996e585cf1417c89d3be2f6c610
Marinessa393 <futurism1303@gmail.com>
2021-04-03 14:09:58 +0300

    add configureStore func

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
-import { createStore, combineReducers } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { configureStore } from '@reduxjs/toolkit';
 import phonebookReducer from './reducer';
 
-const rootReducer = combineReducers({
-  phonebook: phonebookReducer,
+const store = configureStore({
+  reducer: {
+    phonebook: phonebookReducer,
+  },
+  devTools: process.env.NODE_ENV === 'development',
 });
-
-const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
